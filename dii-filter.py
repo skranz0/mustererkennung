1f6a93a9270da62f7c3779c47c1a42dfba62e5fe
skranz <kranz.stefan99@gmail.com>
2023-06-16 19:13:19 +0200

    rough fix negative root and add progress bar

--- dii-filter.py
+++ dii-filter.py
@@ -3,6 +3,7 @@ import scipy as sp
 import skimage
 import sys
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 imgpath = sys.argv[1]
 r = int(sys.argv[2])
@@ -14,13 +15,23 @@ def volume_integral_invariant(img, radius, x, y):
     volume = 0
     for i in range(-radius, radius+1):
         for j in range(-r, r+1):
-            h = img[x+i, y+j] - img[x, y] + np.sqrt(radius**2 - x**2 - y**2)
-            sphere = 2 * np.sqrt(radius**2 - i**2 - j**2)
+            # don't go to negative pixel positions
+            if x+i >= 0 and y+j >= 0:
+                # don't exceed the image
+                if x+i < np.shape(img)[0] and y+j < np.shape(img)[1]:
+                    # make sure there is no sqrt < 0
+                    d = radius**2 - x**2 - y**2
+                    if d >= 0:
+                        h = img[x+i, y+j] - img[x, y] + np.sqrt(d)
+                        sphere = 2 * np.sqrt(d)
+                    else:
+                        h = 0
+                        sphere = 0
 
-            volume += np.max(
-                np.min(h, sphere),
-                0
-            )
+                    volume += np.max(
+                        np.min(h, sphere),
+                        0
+                    )
 
     # normalizing
     volume = (3 * volume) / (2 * np.pi * radius**3) - 1
@@ -29,7 +40,7 @@ def volume_integral_invariant(img, radius, x, y):
 
 def vii_image(img, radius):
     img = np.zeros((img.shape[0], img.shape[1]))
-    for x in range(radius, len(img[0])-radius):
+    for x in tqdm(range(radius, len(img[0])-radius)):
         for y in range(radius, len(img)-radius):
             img[x, y] = volume_integral_invariant(img, radius, x, y)
     return img
