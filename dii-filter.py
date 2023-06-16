42f3f6a0a23bd8ca7cfde31e88ea34e115aab1b9
skranz <kranz.stefan99@gmail.com>
2023-06-16 18:53:01 +0200

    sync both files

--- dii-filter.py
+++ dii-filter.py
@@ -2,22 +2,39 @@ import numpy as np
 import scipy as sp
 import skimage
 import sys
+from matplotlib import pyplot as plt
 
 imgpath = sys.argv[1]
 r = sys.argv[2]
 
-#image = cv2.imread("/Users/skranz/PycharmProjects/mustererkennung/Bilder_itit15_DII_Implementierung/testimage_grid.png", cv2.LOAD_IMAGE_GRAYSCALE)
 image = skimage.io.imread(imgpath, as_gray=True)
 
 
-def volume_integral_invariant(img, r, x, y):
-    V = 0
-    for i in range(-r,r+1):
-        for j in range(-r,r+1):
-            h = img[x+i, y+j] - img[x, y] + np.sqrt(r**2 - x**2 - y**2)
-            sphere = 2 * np.sqrt(r**2 - i**2 - j**2)
+def volume_integral_invariant(img, radius, x, y):
+    volume = 0
+    for i in range(-radius, radius+1):
+        for j in range(-r, r+1):
+            h = img[x+i, y+j] - img[x, y] + np.sqrt(radius**2 - x**2 - y**2)
+            sphere = 2 * np.sqrt(radius**2 - i**2 - j**2)
 
-            V += np.max(
+            volume += np.max(
                 np.min(h, sphere),
                 0
             )
+
+    # normalizing
+    volume = (3 * volume) / (2 * np.pi * radius**3) - 1
+    return volume
+
+
+def vii_image(img, radius):
+    img = np.zeros((img.shape[0], img.shape[1]))
+    for x in range(radius, len(img[0])-radius):
+        for y in range(radius, len(img)-radius):
+            img[x, y] = volume_integral_invariant(img, radius, x, y)
+    return img
+
+
+return_image = vii_image(image, r)
+plt.imshow(return_image)
+plt.show()
