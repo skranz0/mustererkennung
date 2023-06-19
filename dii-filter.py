ca35f4eaacf4c621ba5f2f0559c3ecac410f439d
Stefan Matthias Kranz <stefan.kranz@student.uni-halle.de>
2023-06-19 16:55:35 +0200

    error fix

--- dii-filter.py
+++ dii-filter.py
@@ -13,24 +13,25 @@ print(f"Reading file {imgpath}")
 image = skimage.io.imread(imgpath, as_gray=True)
 #image = checkerboard
 
+
 def volume_integral_invariant(img, radius, x, y):
     volume = 0
     for i in range(-radius, radius + 1):
         for j in range(-radius, radius + 1):
 
             # don't exceed the image
-            if x + i < np.shape(img)[0] and y + j < np.shape(img)[1]:
+            if x + i < np.shape(img)[1] and y + j < np.shape(img)[0]:
                 a = y + j
                 b = x + i
-            elif x + i < np.shape(img)[0] and y + j >= np.shape(img)[1]:
+            elif x + i < np.shape(img)[1] and y + j >= np.shape(img)[0]:
                 # y spiegeln
                 a = y - j
                 b = x + i
-            elif x + i >= np.shape(img)[0] and y + j < np.shape(img)[1]:
+            elif x + i >= np.shape(img)[1] and y + j < np.shape(img)[0]:
                 # x spiegeln
                 a = y + j
                 b = x - i
-            elif x + i >= np.shape(img)[0] and y + j >= np.shape(img)[1]:
+            elif x + i >= np.shape(img)[1] and y + j >= np.shape(img)[0]:
                 # beides spiegeln
                 a = y - j
                 b = x - i
@@ -59,9 +60,9 @@ def volume_integral_invariant(img, radius, x, y):
 
 
 def vii_image(input_img, radius):
-    empty = np.zeros((input_img.shape[1], input_img.shape[0]))
-    for y in tqdm(range(0, len(empty))):
-        for x in range(0, len(empty[0])):
+    empty = np.zeros((input_img.shape[0], input_img.shape[1]))
+    for y in tqdm(range(0, len(empty)-1)):
+        for x in range(0, len(empty[0])-1):
             empty[y, x] = volume_integral_invariant(input_img, radius, x, y)
     return empty
 
