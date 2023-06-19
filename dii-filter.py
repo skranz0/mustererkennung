74a34e2bcecd3da5f69dfa30bf2219d7ab1d6c57
Stefan Matthias Kranz <stefan.kranz@student.uni-halle.de>
2023-06-19 15:51:03 +0200

    edge detections works :D

--- dii-filter.py
+++ dii-filter.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy as sp
 import skimage
 import sys
 from matplotlib import pyplot as plt
@@ -7,45 +6,64 @@ from tqdm import tqdm
 
 imgpath = sys.argv[1]
 r = int(sys.argv[2])
+checkerboard = np.array([[i + j for j in range(5)] for i in range(5)]) % 2
+
 
 print(f"Reading file {imgpath}")
 image = skimage.io.imread(imgpath, as_gray=True)
-print(image)
-
+#image = checkerboard
 
 def volume_integral_invariant(img, radius, x, y):
     volume = 0
     for i in range(-radius, radius + 1):
-        for j in range(-r, r + 1):
-            # don't go to negative pixel positions
-            if x + i >= 0 and y + j >= 0:
-                # don't exceed the image
-                if x + i < np.shape(img)[0] and y + j < np.shape(img)[1]:
-                    # make sure there is no sqrt < 0
-                    d = radius ** 2 - x ** 2 - y ** 2
-                    if d >= 0:
-                        h = img[x + i, y + j] - img[x, y] + np.sqrt(d)
-                        sphere = 2 * np.sqrt(d)
-                    else:
-                        h = 0
-                        sphere = 0
+        for j in range(-radius, radius + 1):
 
-                    volume += np.max(
-                        np.min(h, sphere),
-                        0
-                    )
+            # don't exceed the image
+            if x + i < np.shape(img)[0] and y + j < np.shape(img)[1]:
+                a = y + j
+                b = x + i
+            elif x + i < np.shape(img)[0] and y + j >= np.shape(img)[1]:
+                # y spiegeln
+                a = y - j
+                b = x + i
+            elif x + i >= np.shape(img)[0] and y + j < np.shape(img)[1]:
+                # x spiegeln
+                a = y + j
+                b = x - i
+            elif x + i >= np.shape(img)[0] and y + j >= np.shape(img)[1]:
+                # beides spiegeln
+                a = y - j
+                b = x - i
 
+            def calc_h():
+                if radius ** 2 - x ** 2 - y ** 2 >= 0:
+                    return img[abs(a), abs(b)] - img[y, x] + np.sqrt(radius ** 2 - x ** 2 - y ** 2)
+                else:
+                    return img[abs(a), abs(b)] - img[y, x]
+
+            def calc_sphere():
+                if radius ** 2 - i ** 2 - j ** 2 >= 0:
+                    return 2 * np.sqrt(radius ** 2 - i ** 2 - j ** 2)
+                else:
+                    return 0
+
+            h = calc_h()
+            sphere = calc_sphere()
+
+            tmp = min(h, sphere)
+            volume += max(tmp, 0)
     # normalizing
-    volume = (3 * volume) / (2 * np.pi * radius ** 3) - 1
+    if volume != 0:
+        volume = (3 * volume) / (2 * np.pi * radius ** 3) - 1
     return volume
 
 
 def vii_image(input_img, radius):
-    img = np.zeros((input_img.shape[0], input_img.shape[1]))
-    for x in tqdm(range(radius, len(img[0]) - radius), unit="pixelrows"):
-        for y in range(radius, len(img) - radius):
-            img[x, y] = volume_integral_invariant(input_img, radius, x, y)
-    return img
+    empty = np.zeros((input_img.shape[1], input_img.shape[0]))
+    for y in tqdm(range(0, len(empty))):
+        for x in range(0, len(empty[0])):
+            empty[y, x] = volume_integral_invariant(input_img, radius, x, y)
+    return empty
 
 
 return_image = vii_image(image, r)
